1a0688f8ca08048e0fa2fc9c2776680ede0131a5
Joel Fragoso <joelfragoso85@icloud.com>
2020-08-22 18:27:10 -0300

    feat: add state to signin

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useCallback, useState, FormEvent } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { StoreState } from '../../store/createStore';
+import { signInRequest } from '../../store/modules/auth/actions';
+
+interface ISignInValues {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { isSignedIn, loadingSignInRequest } = useSelector((state: StoreState) => state.auth);
+  const dispatch = useDispatch();
+
+  console.log(loadingSignInRequest);
+
+  const handleSubmit = useCallback((event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    if (!isSignedIn) {
+      dispatch(signInRequest({
+        email,
+        password,
+      }));
+    }
+  }, [email, password, isSignedIn, dispatch]);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Faça o seu logon</h1>
-      <input defaultValue="johndoe@example.com" />
-      <input type="password" defaultValue="secret" />
+      <input
+        name="email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        defaultValue={'johndoe@example.com'}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        defaultValue={'secret'}
+      />
       <button type="submit">Entrar</button>
     </form>
   );
